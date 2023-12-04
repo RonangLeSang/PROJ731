7d4a8fb1cd115237eb292322692a019071968a09
ronanter <ronan1terras@gmail.com>
2023-12-04 15:14:07 +0100

    [FEATURE] reducer and word counter functions added

--- Main.java
+++ Main.java
@@ -1,9 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main{
 
     public static void main(String[] args){
+        String text = "bonjour je suis Andres et je suis un gros naze";
 
+        WordCounter wc = new WordCounter(text);
+        System.out.println(wc.count());
 
-        System.out.println("feur");
+        HashMap<String, Integer> map1 = new HashMap<>();
+        map1.put("A", 1);
+        map1.put("B", 2);
+        map1.put("D", 8);
+
+        HashMap<String, Integer> map2 = new HashMap<>();
+        map2.put("B", 3);
+        map2.put("C", 4);
+        map2.put("E", 7);
+
+        HashMap<String, Integer> map3 = new HashMap<>();
+        map3.put("A", 1);
+        map3.put("C", 4);
+        map3.put("E", 2);
+
+        ArrayList<HashMap<String, Integer>> list = new ArrayList<>();
+        list.add(map1);
+        list.add(map2);
+        list.add(map3);
+
+        Reducer r = new Reducer(list);
+        System.out.println(r.testMerge());
 
     }
 
--- WordCounter.java
+++ WordCounter.java
@@ -1,13 +1,23 @@
+import java.util.HashMap;
+
 public class WordCounter {
 
-    private String text;
+    private String[] text;
+    private HashMap<String, Integer> wordCount = new HashMap<String, Integer>();
 
-    public WordCounter(){
-        this.text = text;
+    public WordCounter(String text){
+        this.text = text.split(" ");;
     }
 
-    @Override
-    public String toString() {
-        return text;
+    public HashMap count(){
+        for(String word : text){
+            if(wordCount.containsKey(word)){
+                wordCount.put(word, wordCount.get(word)+1);
+            }else{
+                wordCount.put(word, 1);
+            }
+        }
+        return wordCount;
     }
+
 }
